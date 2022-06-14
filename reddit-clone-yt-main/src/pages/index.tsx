6a9d30788733590eeefc9e7a18a94f61aaf22228
Obada Sawalmeh <97031203+sawalmeh4@users.noreply.github.com>
2022-06-14 21:07:31 +0300

    Add files via upload

---
+++ reddit-clone-yt-main/src/pages/index.tsx
@@ -0,0 +1,218 @@
+import { useEffect } from "react";
+import { Stack } from "@chakra-ui/react";
+import {
+  collection,
+  DocumentData,
+  getDocs,
+  limit,
+  onSnapshot,
+  orderBy,
+  query,
+  QuerySnapshot,
+  where,
+} from "firebase/firestore";
+import type { NextPage } from "next";
+import { useRouter } from "next/router";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useRecoilValue } from "recoil";
+import { communityState } from "../atoms/communitiesAtom";
+import { Post, PostVote } from "../atoms/postsAtom";
+import CreatePostLink from "../components/Community/CreatePostLink";
+import Recommendations from "../components/Community/Recommendations";
+import PageContentLayout from "../components/Layout/PageContent";
+import PostLoader from "../components/Post/Loader";
+import PostItem from "../components/Post/PostItem";
+import { auth, firestore } from "../firebase/clientApp";
+import usePosts from "../hooks/usePosts";
+import Premium from "../components/Community/Premium";
+import PersonalHome from "../components/Community/PersonalHome";
+
+const Home: NextPage = () => {
+  const [user, loadingUser] = useAuthState(auth);
+  const {
+    postStateValue,
+    setPostStateValue,
+    onVote,
+    onSelectPost,
+    onDeletePost,
+    loading,
+    setLoading,
+  } = usePosts();
+  const communityStateValue = useRecoilValue(communityState);
+
+  const getUserHomePosts = async () => {
+    console.log("GETTING USER FEED");
+    setLoading(true);
+    try {
+      const feedPosts: Post[] = [];
+
+      if (communityStateValue.mySnippets.length) {
+        console.log("GETTING POSTS IN USER COMMUNITIES");
+
+        const myCommunityIds = communityStateValue.mySnippets.map(
+          (snippet) => snippet.communityId
+        );
+        let postPromises: Array<Promise<QuerySnapshot<DocumentData>>> = [];
+        [0, 1, 2].forEach((index) => {
+          if (!myCommunityIds[index]) return;
+
+          postPromises.push(
+            getDocs(
+              query(
+                collection(firestore, "posts"),
+                where("communityId", "==", myCommunityIds[index]),
+                limit(3)
+              )
+            )
+          );
+        });
+        const queryResults = await Promise.all(postPromises);
+
+        queryResults.forEach((result) => {
+          const posts = result.docs.map((doc) => ({
+            id: doc.id,
+            ...doc.data(),
+          })) as Post[];
+          feedPosts.push(...posts);
+        });
+      }
+      else {
+        console.log("USER HAS NO COMMUNITIES - GETTING GENERAL POSTS");
+
+        const postQuery = query(
+          collection(firestore, "posts"),
+          orderBy("voteStatus", "desc"),
+          limit(10)
+        );
+        const postDocs = await getDocs(postQuery);
+        const posts = postDocs.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        })) as Post[];
+        feedPosts.push(...posts);
+      }
+
+      console.log("HERE ARE FEED POSTS", feedPosts);
+
+      setPostStateValue((prev) => ({
+        ...prev,
+        posts: feedPosts,
+      }));
+
+    } catch (error: any) {
+      console.log("getUserHomePosts error", error.message);
+    }
+    setLoading(false);
+  };
+
+  const getNoUserHomePosts = async () => {
+    console.log("GETTING NO USER FEED");
+    setLoading(true);
+    try {
+      const postQuery = query(
+        collection(firestore, "posts"),
+        orderBy("voteStatus", "desc"),
+        limit(10)
+      );
+      const postDocs = await getDocs(postQuery);
+      const posts = postDocs.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      console.log("NO USER FEED", posts);
+
+      setPostStateValue((prev) => ({
+        ...prev,
+        posts: posts as Post[],
+      }));
+    } catch (error: any) {
+      console.log("getNoUserHomePosts error", error.message);
+    }
+    setLoading(false);
+  };
+
+  const getUserPostVotes = async () => {
+    const postIds = postStateValue.posts.map((post) => post.id);
+    const postVotesQuery = query(
+      collection(firestore, `users/${user?.uid}/postVotes`),
+      where("postId", "in", postIds)
+    );
+    const unsubscribe = onSnapshot(postVotesQuery, (querySnapshot) => {
+      const postVotes = querySnapshot.docs.map((postVote) => ({
+        id: postVote.id,
+        ...postVote.data(),
+      }));
+
+      setPostStateValue((prev) => ({
+        ...prev,
+        postVotes: postVotes as PostVote[],
+      }));
+    });
+
+    return () => unsubscribe();
+  };
+
+  useEffect(() => {
+    if (!communityStateValue.initSnippetsFetched) return;
+
+    if (user) {
+      getUserHomePosts();
+    }
+  }, [user, communityStateValue.initSnippetsFetched]);
+
+  useEffect(() => {
+    if (!user && !loadingUser) {
+      getNoUserHomePosts();
+    }
+  }, [user, loadingUser]);
+
+  useEffect(() => {
+    if (!user?.uid || !postStateValue.posts.length) return;
+    getUserPostVotes();
+
+    return () => {
+      setPostStateValue((prev) => ({
+        ...prev,
+        postVotes: [],
+      }));
+    };
+  }, [postStateValue.posts, user?.uid]);
+
+  return (
+    <PageContentLayout>
+      <>
+        <CreatePostLink />
+        {loading ? (
+          <PostLoader />
+        ) : (
+          <Stack>
+            {postStateValue.posts.map((post: Post, index) => (
+              <PostItem
+                key={post.id}
+                post={post}
+                postIdx={index}
+                onVote={onVote}
+                onDeletePost={onDeletePost}
+                userVoteValue={
+                  postStateValue.postVotes.find(
+                    (item) => item.postId === post.id
+                  )?.voteValue
+                }
+                userIsCreator={user?.uid === post.creatorId}
+                onSelectPost={onSelectPost}
+                homePage
+              />
+            ))}
+          </Stack>
+        )}
+      </>
+      <Stack spacing={5} position="sticky" top="14px">
+        <Recommendations />
+        <Premium />
+        <PersonalHome />
+      </Stack>
+    </PageContentLayout>
+  );
+};
+
+export default Home;
